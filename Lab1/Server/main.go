f569fe8957b84feff33fda35e4377f3808641e4f
ChangQi-555 <qi@LAPTOP-AIRP6E66.localdomain>
2022-11-11 00:58:25 +0100

    update request

--- Lab1/Server/main.go
+++ Lab1/Server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"net"
 	"net/http"
@@ -47,12 +49,13 @@ func ListenAndServe(address string, root string) error {
 }
 
 func handleConnection(conn net.Conn, root string) {
+	//Create an empty buffer
 	buffer := make([]byte, 1024)
 	defer conn.Close()
 	for {
 		// read from connection
 		msg, err := conn.Read(buffer)
-
+		fmt.Println("Read from connection: ", string(buffer[:msg]))
 		if err != nil {
 			// handle error
 			if err.Error() == "EOF" {
@@ -66,12 +69,14 @@ func handleConnection(conn net.Conn, root string) {
 		fmt.Println(conn.RemoteAddr().String())
 
 		// msg to request
-		// request := string(buffer[:msg])
-		request := http.ReadRequest(buffer)
+		request, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(buffer[:msg])))
 		fmt.Println("Request content:\n", request)
 
-		// Todo: handle request with function handleRequest, only GET and POST. Other methods should return 405.
-		// response := handleRequest(request, root)
+		// TODO: handle request with function handleRequest, only GET and POST. Other methods should return 405.
+		// TODO: how to create a response writer?
+		w := http.ResponseWriter{}
+		response := handleRequest(w, request, root)
+		fmt.Println("Response content:\n", response)
 
 		/*
 			Todo:
@@ -85,9 +90,32 @@ func handleConnection(conn net.Conn, root string) {
 	}
 }
 
+func handleRequest(w http.ResponseWriter, request *http.Request, root string) {
+	w.Header().Set("Content-Type", "text/plain")
+	if request.Method == "GET" {
+		//Return the content of the file
+		path := request.URL.Path
+		// open file and read content to buffer
+		_, err := os.Open(root + path)
+		if err != nil {
+			fmt.Println("Open file error: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			fmt.Println("File exists")
+			w.WriteHeader(http.StatusOK)
+			// w.Write([]byte(file))
+		}
+	} else if request.Method == "POST" {
+
+	} else {
+		// Response "Not Implemented" (501)
+		fmt.Println("invalid method")
+	}
+}
 func main() {
 	port := getPort()
 	if port == -1 {
+		fmt.Println("Please state port number!")
 		return
 	}
 	addr := "127.0.0.1:" + strconv.Itoa(port)
