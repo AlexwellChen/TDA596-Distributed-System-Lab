702a0ae3e7be1f58554d6ec303b3042b45f456e3
Alexwell <935562189@qq.com>
2022-11-10 16:34:15 +0100

    Update

--- Lab1/Client/main.go
+++ Lab1/Client/main.go
@@ -36,16 +36,17 @@ func sender(conn *net.TCPConn) {
 	url := "http://" + host_addr + "/root/1.txt"
 	fmt.Println("url:", url)
 	request, _ := http.NewRequest("GET", url, nil)
-	fmt.Println("Request: ", request.Header)
-	// src := request.Header
+	err := request.Write(conn)
+	// fmt.Println("Request: ", request)
+	// src := request.URL.String()             // Todo: 把request转换成字符串
 	// msgBack, err := conn.Write([]byte(src)) //给服务器发信息
 
-	// if err != nil {
-	// 	fmt.Println(conn.RemoteAddr().String(), " Error: ", err)
-	// 	os.Exit(1)
-	// }
-	// buffer := make([]byte, 1024)
-	// msg, err := conn.Read(buffer) //接受服务器信息
-	// fmt.Println(conn.RemoteAddr().String(), "服务器反馈：", string(buffer[:msg]), msgBack)
+	if err != nil {
+		fmt.Println(conn.RemoteAddr().String(), " Error: ", err)
+		os.Exit(1)
+	}
+	buffer := make([]byte, 1024)
+	msg, _ := conn.Read(buffer) //接受服务器信息
+	fmt.Println(conn.RemoteAddr().String(), "服务器返回: ", string(buffer[:msg]))
 	// // conn.Write([]byte("ok")) //在告诉服务器，它的反馈收到了。
 }
--- Lab1/Server/main.go
+++ Lab1/Server/main.go
@@ -61,44 +61,27 @@ func handleConnection(conn net.Conn, root string) {
 			return
 		}
 		// print message
-		fmt.Print(conn.RemoteAddr().String())
+		fmt.Println(conn.RemoteAddr().String())
 
 		// msg to request
 		request := string(buffer[:msg])
-		fmt.Println("request:", request)
-		fmt.Println("--------------------")
+		fmt.Println("Request content:\n", request)
 
 		// handle request
 		// response := handleRequest(request, root)
 
 		// response to connection
 		conn.Write([]byte("Test response ack"))
-
-		// write to connection
-		// bufferReturn := "我收到了"
-		// msgW, errW := conn.Write([]byte(bufferReturn))
-
-		// // handle error
-		// if errW != nil {
-		// 	fmt.Print(conn.RemoteAddr().String(), msgW)
-		// 	fmt.Println("没有收到回执")
-		// 	return
-		// }
-
-		// // Revc ack
-		// msg, err = conn.Read(buffer)
-		// fmt.Println(conn.RemoteAddr().String(), "客户端收到回执", string(buffer[:msg]), "客户收到了", msgW, "；实际发送了", len(bufferReturn))
+		fmt.Println("--------------------")
 	}
 }
 
 func main() {
-	port := getPort()
+	port := 8080
 	if port == -1 {
 		return
 	}
 	addr := "127.0.0.1:" + strconv.Itoa(port)
 	root := "./root"
-	// http.Handle("/", &TestHandler{"Hi"}) //根路由
-	// http.HandleFunc("/test", SayHello)   //test路由
 	ListenAndServe(addr, root)
 }
