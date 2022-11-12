1214a4188db16301bd72105ab4739ed745a53f61
Juvenilesense <50232970+Juvenilesense@users.noreply.github.com>
2022-11-12 15:56:37 +0100

    Add files via upload

--- Lab1/Client/main.go
+++ Lab1/Client/main.go
@@ -15,8 +15,8 @@ func main() {
 	fmt.Println("Please enter server address:")
 	reader := bufio.NewReader(os.Stdin)
 	server, _ := reader.ReadString('\n')
-	// server := "localhost:8080"
-	// fmt.Println(strings.TrimSpace(server))
+	//server := "localhost:8080"
+	//fmt.Println(strings.TrimSpace(server))
 	server = strings.TrimSpace(server)
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", server)
 
@@ -38,19 +38,38 @@ func main() {
 	for {
 		//repeat send request until user input "exit"
 		//Ask user for input request resource and method?
-		fmt.Println("Please enter request method:")
+		fmt.Println("Please enter request method:") //GET POST
 		method, _ := reader.ReadString('\n')
 		method = strings.TrimSpace(method)
-		fmt.Println("Please enter request resource:")
+		fmt.Println("Please enter request resource root:") //root
 		resource, _ := reader.ReadString('\n')
-		resource = strings.TrimSpace(resource)
-		sender(conn, method, resource)
+		//resource = strings.TrimSpace(resource)
+		resource = "/" + strings.TrimSpace(resource)
+		fmt.Println("Please enter request file:") // file name
+		fileName, _ := reader.ReadString('\n')
+		fileName = strings.TrimSpace(fileName)
+		//if fileName == "" {
+		//	resourcePath := resource
+		//	sender(conn, method, resourcePath, fileName)
+		//} else {
+		//	resourcePath := resource + "/" + fileName
+		//	sender(conn, method, resourcePath, fileName)
+		//}
+		resourcePath := resource + "/" + fileName
+
+		sender(conn, method, resourcePath, fileName)
 		fmt.Println("send over")
 	}
+	/*	method := "GET"
+		file := "2.jpg"
+		resource := "/root/" + file
+		sender(conn, method, resource, file)
+		//sender(conn, method, resource)
+		fmt.Println("send over")*/
 
 }
 
-func sender(conn *net.TCPConn, method string, resource string) {
+func sender(conn *net.TCPConn, method string, resource string, fileName string) {
 	host_addr := conn.RemoteAddr().String()
 
 	url := "http://" + host_addr + resource
@@ -74,12 +93,24 @@ func sender(conn *net.TCPConn, method string, resource string) {
 
 	// Handle response body
 	// Todo: 区分请求内容，如果是路径，需要列出目录下的文件；如果是文件，需要读取文件内容并保存到本地
-	//需要区分文件后缀类型..需要修改
+	if fileName == "" {
+		fmt.Println("Response status:", response.Status)
+		fmt.Println("The files in the directory have been listed below:")
+		_, _ = io.Copy(os.Stdout, response.Body)
+	} else {
+		fmt.Println("Response Header content type:", response.Header.Get("Content-Type"))
+		downloadFile(response, fileName)
+	}
+	//downloadFile(response, fileName)
+}
+
+func downloadFile(response *http.Response, fileName string) {
+	//Based on the response.StatueCode, we can decide whether to download the file
 	if response.StatusCode == 200 {
 		fmt.Println("Response status:", response.Status)
 		fmt.Println("Response body:")
 		//create file
-		file, err := os.Create("Hello world.txt")
+		file, err := os.Create(fileName)
 		if err != nil {
 			fmt.Println("Error create file:", err)
 		}
@@ -90,25 +121,12 @@ func sender(conn *net.TCPConn, method string, resource string) {
 		fmt.Println(copyFile)
 	} else if response.StatusCode == 404 {
 		fmt.Println("Response status:", response.Status)
+		fmt.Println("Not Found")
+	} else if response.StatusCode == 400 {
+		fmt.Println("Response status:", response.Status)
 		fmt.Println("Bad Request")
 	} else if response.StatusCode == 501 {
 		fmt.Println("Response status:", response.Status)
 		fmt.Println("Not Implemented")
 	}
-
-	/*	body, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			fmt.Println("Error reading body:", err)
-		}
-		fmt.Println("Response body:\n", string(body))*/
-	/*	list := url
-		fmt.Println(list)
-		filePath := list[1]
-		fmt.Println("filepath:", filePath)
-		info, err := os.Stat(string(filePath))
-		if err != nil {
-			fmt.Println("Error reading file:", err)
-		}
-		filename := info.Name()
-		fmt.Println("filename:", filename)*/
 }
