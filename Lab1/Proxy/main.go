03a5d09e346f7c0e173e0b0c7d866a38b2e72616
Alexwell <935562189@qq.com>
2022-11-14 11:41:24 +0100

    Update proxy
    Finish proxy

--- Lab1/Proxy/main.go
+++ Lab1/Proxy/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"net/url"
 	"strings"
 )
@@ -16,9 +19,11 @@ func main() {
 		fmt.Println("Error listening:", err.Error())
 	}
 	defer listener.Close()
+	fmt.Println("Start Listening ...")
 	for {
-		fmt.Println("Waiting for a connection ...")
+
 		client, err := listener.Accept()
+
 		if err != nil {
 			fmt.Println("Error accept client: ", err.Error())
 		}
@@ -27,51 +32,133 @@ func main() {
 }
 
 func handleClientRequest(client net.Conn) {
-	if client == nil {
-		return
+	for {
+		if client == nil {
+			return
+		}
+		var buffer [1024]byte
+		client_request_msg, err := client.Read(buffer[:])
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Client closed connection")
+				defer client.Close()
+				fmt.Println("*******************************************************")
+				fmt.Println("--------------------Proxy over here--------------------")
+				fmt.Println("*******************************************************")
+			} else {
+				fmt.Println("Error reading client buffer:", err.Error())
+			}
+			return
+		}
+
+		var method, host, address string
+		fmt.Sscanf(string(buffer[:bytes.IndexByte(buffer[:], '\n')]), "%s%s", &method, &host)
+
+		//Construct client request
+		client_request, err := http.ReadRequest(bufio.NewReader(bytes.NewBuffer(buffer[:client_request_msg])))
+		if err != nil {
+			fmt.Println("Error reading client request:", err.Error())
+			return
+		}
+
+		//Construct client_request's Response
+		client_request.Response = new(http.Response)
+
+		// If method is not GET, return 501
+		if method != "GET" {
+			client_request.Response.StatusCode = http.StatusNotImplemented
+			client_request.Response.Write(client)
+			return
+		}
+
+		hostPortURL, err := url.Parse(host)
+		if err != nil {
+			// Return internal server error
+			client_request.Response.StatusCode = http.StatusInternalServerError
+			client_request.Response.Write(client)
+			fmt.Println("Error parsing host:", err.Error())
+			return
+		}
+
+		//http访问
+		if !strings.Contains(hostPortURL.Host, ":") {
+			//host不带端口， 默认8080
+			address = hostPortURL.Host + ":8080"
+		} else {
+			address = hostPortURL.Host
+		}
+
+		//获得了请求的host和port，就开始拨号吧
+		server, err := net.Dial("tcp", address)
+		if err != nil {
+			// Return internal server error
+			client_request.Response.StatusCode = http.StatusInternalServerError
+			client_request.Response.Write(client)
+			fmt.Println("Error dialing server:", err.Error())
+			return
+		}
+		defer server.Close()
+
+		// Construct proxy to server request
+		server_request, err := http.NewRequest("GET", host, nil)
+		if err != nil {
+			// Return internal server error
+			client_request.Response.StatusCode = http.StatusInternalServerError
+			client_request.Response.Write(client)
+			fmt.Println("Error constructing server request:", err.Error())
+			return
+		}
+
+		// Copy client request header to server request
+		for k, v := range client_request.Header {
+			server_request.Header[k] = v
+		}
+
+		// Send server request
+		err = server_request.Write(server)
+		if err != nil {
+			// Return internal server error
+			client_request.Response.StatusCode = http.StatusInternalServerError
+			client_request.Response.Write(client)
+			fmt.Println("Error writing server request:", err.Error())
+			return
+		}
+
+		// Read server response
+		server_response, err := http.ReadResponse(bufio.NewReader(server), server_request)
+		if err != nil {
+			// Return internal server error
+			client_request.Response.StatusCode = http.StatusInternalServerError
+			client_request.Response.Write(client)
+			fmt.Println("Error reading server response:", err.Error())
+			return
+		}
+		defer server_response.Body.Close()
+
+		// Copy server response body to client response
+		client_request.Response.StatusCode = server_response.StatusCode
+		content, err := ioutil.ReadAll(server_response.Body)
+
+		// fmt.Println("------------------Content test------------------")
+		// fmt.Println("content:", string(content))
+
+		if err != nil {
+			// Return internal server error
+			client_request.Response.StatusCode = http.StatusInternalServerError
+			client_request.Response.Write(client)
+			fmt.Println("Error reading server response body:", err.Error())
+			return
+		}
+		client_request.Response.ContentLength = int64(len(content))
+		client_request.Response.Body = ioutil.NopCloser(strings.NewReader(string(content)))
+		// Send client response
+		err = client_request.Response.Write(client)
+		if err != nil {
+			fmt.Println("Error writing client response:", err.Error())
+			return
+		}
+
+		defer client_request.Response.Body.Close()
 	}
-	var buffer [1024]byte
-	client_request_msg, err := client.Read(buffer[:])
-	if err != nil {
-		fmt.Println("Error reading client buffer:", err.Error())
-		return
-	}
 
-	var method, host, address string
-	fmt.Sscanf(string(buffer[:bytes.IndexByte(buffer[:], '\n')]), "%s%s", &method, &host)
-
-	hostPortURL, err := url.Parse(host)
-	if err != nil {
-		fmt.Println("Error parsing host:", err.Error())
-		return
-	}
-
-	//http访问
-	if !strings.Contains(hostPortURL.Host, ":") {
-		//host不带端口， 默认8080
-		address = hostPortURL.Host + ":8080"
-	} else {
-		address = hostPortURL.Host
-	}
-
-	//获得了请求的host和port，就开始拨号吧
-	server, err := net.Dial("tcp", address)
-	if err != nil {
-		fmt.Println("Error dialing server:", err.Error())
-		return
-	}
-	defer server.Close()
-
-	// Forwarding client request to server
-	server.Write(buffer[:client_request_msg])
-
-	// Data transfer between client and server
-	go io.Copy(server, client)
-	io.Copy(client, server)
-	// Todo: Close connection when client is closed
-
-	// Unable to reach here
-	client.Close()
-	fmt.Println("Connection closed")
-	fmt.Println("--------------------------------------------------")
 }
