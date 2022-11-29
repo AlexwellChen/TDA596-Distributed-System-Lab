f71e4e25325075b6642b9762169c38e1cfb9e866
Alexwell <935562189@qq.com>
2022-11-29 10:51:06 +0100

    Update tools.go

--- Lab2/Client/tools.go
+++ Lab2/Client/tools.go
@@ -109,6 +109,23 @@ func (node *Node) leaveChord() {
 	// For failure handling, backup the data in the bucket to the successor (Bonus)
 }
 
+func (node *Node) printState() {
+	// Print current node state
+	fmt.Println("-------------- Current Node State ------------")
+	fmt.Println("Node Name: ", node.Name)
+	fmt.Println("Node Address: ", node.Address)
+	fmt.Println("Node Identifier: ", node.Identifier)
+	fmt.Println("Node Predecessor: ", node.Predecessor)
+	fmt.Println("Node Successors: ")
+	for i := 0; i < len(node.Successors); i++ {
+		fmt.Println(node.Successors[i])
+	}
+	fmt.Println("Node Finger Table: ")
+	for i := 0; i < fingerTableSize; i++ {
+		fmt.Println(node.FingerTable[i])
+	}
+}
+
 /*------------------------------------------------------------*/
 /*                  Comm Interface By: Alexwell               */
 /*------------------------------------------------------------*/
@@ -190,16 +207,16 @@ type FindSuccessorRPCReply struct {
 * @return: 		found: whether the key is found
 * 				successor: the successor of the key
  */
-func (node *Node) FindSuccessorRPC(requestID string, reply *FindSuccessorRPCReply) error {
+func (node *Node) FindSuccessorRPC(requestID *big.Int, reply *FindSuccessorRPCReply) error {
 	fmt.Println("-------------- Invoke FindSuccessor_RPC function ------------")
 	reply.found, reply.SuccessorAddress = node.findSuccessor(requestID)
 	return nil
 }
 
 // Local use function
-func (node *Node) findSuccessor(requestID string) (bool, NodeAddress) {
+func (node *Node) findSuccessor(requestID *big.Int) (bool, NodeAddress) {
 	fmt.Println("*************** Invoke findSuccessor function ***************")
-	if between(node.Identifier, strHash(requestID), strHash(string(node.Successors[0])), true) {
+	if between(node.Identifier, requestID, strHash(string(node.Successors[0])), true) {
 		return true, node.Successors[0]
 	} else {
 		return false, node.closePrecedingNode(requestID)
@@ -207,11 +224,11 @@ func (node *Node) findSuccessor(requestID string) (bool, NodeAddress) {
 }
 
 // Local use function
-func (node *Node) closePrecedingNode(requestID string) NodeAddress {
+func (node *Node) closePrecedingNode(requestID *big.Int) NodeAddress {
 	fmt.Println("************ Invoke closePrecedingNode function ************")
 	fingerTableSize := len(node.FingerTable)
 	for i := fingerTableSize - 1; i >= 1; i-- {
-		if between(node.Identifier, strHash(string(node.FingerTable[i])), strHash(requestID), true) {
+		if between(node.Identifier, strHash(string(node.FingerTable[i])), requestID, true) {
 			return node.FingerTable[i]
 		}
 	}
@@ -219,14 +236,13 @@ func (node *Node) closePrecedingNode(requestID string) NodeAddress {
 }
 
 // Local use function
-func find(id string, startNode NodeAddress) NodeAddress {
+func find(id *big.Int, startNode NodeAddress) NodeAddress {
 	fmt.Println("****************** Invoke find function *********************")
 	found := false
 	nextNode := startNode
 	i := 0
 	maxSteps := 10
 	for !found && i < maxSteps {
-		// Todo: Send request to nextNode, execute FindSuccessor(id), and return the result
 		// found, nextNode = nextNode.FindSuccessor(id)
 		result := FindSuccessorRPCReply{}
 		err := ChordCall(nextNode, "Node.FindSuccessorRPC", id, &result)
